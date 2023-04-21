08c588e9b20dbc8dfbe7e3ec8ccbeb756a0bdcc8
Ari Frid <arigfrid@hotmail.com>
2023-04-20 21:56:17 -0700

    consolidated dummy data into one file in lib

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -2,8 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nutrition_app/screens/general_settings.dart';
 import 'package:nutrition_app/screens/ingredients_page.dart';
-import 'package:nutrition_app/screens/ingredients_page.dart' as ing show ingredients;
 import 'package:nutrition_app/screens/meal_page.dart';
+
+import '../temp_dummy_data.dart';
 
 class IndexPage extends StatelessWidget {
   const IndexPage({Key? key}) : super(key: key);
@@ -50,7 +51,7 @@ class IndexPage extends StatelessWidget {
               children: [
                 ListTile(title: Text('Confirm Ingredient Debug'), onTap: (){
                   showDialog(context: context, builder: (context)=>
-                     confirmIngredient(ing.ingredients[1], context)
+                     confirmIngredient(ingredients[1], context)
                   );}, tileColor: Colors.green),
                 ListTile(title: Text(''), onTap: (){}, tileColor: Colors.green),
                 ListTile(title: Text(''), onTap: (){}, tileColor: Colors.green),
