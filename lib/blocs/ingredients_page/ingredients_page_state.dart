19fdb68306dfc231b7132381f948af07573be250
Ari Frid <arigfrid@hotmail.com>
2023-05-28 01:00:33 -0700

    added func ing on tile tap to bloc

--- lib/blocs/ingredients_page/ingredients_page_state.dart
+++ lib/blocs/ingredients_page/ingredients_page_state.dart
@@ -5,38 +5,45 @@ class IngredientsPageState {
   bool includeSubRecipes;
   List<MealComponentFactory> searchResults;
   String currentText = '';
-  Function(BuildContext context, MealComponentFactory ingredient)? onTileTap;
+  void Function(BuildContext context, MealComponentFactory ingredient)? onTileTap;
 
-  factory IngredientsPageState.initial(App app, [bool? include]) {
+  factory IngredientsPageState.initial(App app, [bool? include, Function(BuildContext context, MealComponentFactory ingredient)? onTileTap]) {
     return IngredientsPageState(
         app: app,
         includeSubRecipes: toBool(include),
         searchResults: toBool(include)
             ? app.ingredients
-            : app.baseIngredients.values.toList());
+            : app.baseIngredients.values.toList(),
+        onTileTap: onTileTap
+    );
   }
   factory IngredientsPageState.fromState(IngredientsPageState state) =>
-    IngredientsPageState(app: state.app, includeSubRecipes: state.includeSubRecipes, searchResults: state.searchResults, currentText: state.currentText);
+    IngredientsPageState(app: state.app, includeSubRecipes: state.includeSubRecipes, searchResults: state.searchResults, currentText: state.currentText, onTileTap: state.onTileTap);
 
 
   IngredientsPageState({
     required this.app,
     required this.includeSubRecipes,
     required this.searchResults,
-    String? currentText
+    String? currentText,
+    this.onTileTap
   }){if (currentText != null){this.currentText = currentText;}}
 
   IngredientsPageState copyWith({
     App? app_,
     bool? includeSubRecipes_,
     List<MealComponentFactory>? searchResults_,
-    String? currentText_
+    String? currentText_,
+    Function(BuildContext context, MealComponentFactory ingredient)? onTileTap,
+
+
   }) {
     return IngredientsPageState(
         app: app_ ?? app,
         includeSubRecipes: includeSubRecipes_ ?? includeSubRecipes,
         searchResults: searchResults_ ?? List.from(searchResults),
-        currentText: currentText_ ?? currentText
+        currentText: currentText_ ?? currentText,
+        onTileTap: onTileTap ?? this.onTileTap
     );
   }
 }
@@ -47,7 +54,8 @@ class IngPageSuccessfulCreation extends IngredientsPageState {
     required super.app,
     required super.includeSubRecipes,
     required super.searchResults,
-    required this.newIngredient
+    required this.newIngredient,
+    required super.onTileTap
   });
 
   factory IngPageSuccessfulCreation.fromState(
@@ -56,7 +64,8 @@ class IngPageSuccessfulCreation extends IngredientsPageState {
           app: state.app,
           includeSubRecipes: state.includeSubRecipes,
           searchResults: state.searchResults,
-          newIngredient: newIngredient
+          newIngredient: newIngredient,
+          onTileTap: state.onTileTap
       );
 
 }
@@ -68,10 +77,11 @@ class IngPageApiError extends IngredientsPageState{
     required super.app,
     required super.includeSubRecipes,
     required super.searchResults,
-    required super.currentText
+    required super.currentText,
+    required super.onTileTap
   });
   factory IngPageApiError.fromState(IngredientsPageState state, String message) =>
-      IngPageApiError(app: state.app, includeSubRecipes: state.includeSubRecipes, searchResults: state.searchResults, message: message, currentText: state.currentText);
+      IngPageApiError(app: state.app, includeSubRecipes: state.includeSubRecipes, searchResults: state.searchResults, message: message, currentText: state.currentText, onTileTap: state.onTileTap);
 }
 
 // class IngredientsPageInitial extends IngredientsPageState {}
