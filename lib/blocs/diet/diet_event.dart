c76f8cf991e24edabecbcf4735e2065000457ab7
Ari Frid <arigfrid@hotmail.com>
2023-05-30 16:26:27 -0700

    planned events for diet bloc

--- lib/blocs/diet/diet_event.dart
+++ lib/blocs/diet/diet_event.dart
@@ -6,8 +6,12 @@ abstract class DietEvent {}
 class RefreshDietEvent extends DietEvent{}
 void refreshDiet(BuildContext context){context.read<DietBloc>().add(RefreshDietEvent());}
 class AddDay extends DietEvent{}
-// class x extends DietEvent{}
-// class x extends DietEvent{}
-// class x extends DietEvent{}
-// class dx extends DietEvent{}
+// class AddMealToDay extends DietEvent{}
+// class MealUpdateGrams extends DietEvent{}
+// class DeleteMealFromDay extends DietEvent{}
+// class AddMealToDay extends DietEvent{}
+// class ReorderMealInDay extends DietEvent{} //
+// class ReOrderDay extends DietEvent{}
+// class DuplicateDay extends DietEvent{}
+// class DeleteDay extends DietEvent{}
 // class dx extends DietEvent{}
